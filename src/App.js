c1d07e6b5d2aa55c34ebe111d7f2c783afd740ab
andrewtjong <andrewttj@yahoo.com>
2020-11-09 16:27:32 +0100

    use data in context state and map over photos to display the images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react"
+import {Switch, Route} from "react-router-dom"
+
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
-import {Switch, Route} from "react-router-dom"
 
 function App() {    
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <h1>Homepage</h1>
             <Switch>
               <Route exact path="/"> <Photos /> </Route>
-              <Route exact path="/cart"> <Cart /> </Route>
+              <Route path="/cart"> <Cart /> </Route>
             </Switch>
         </div>
         
--- src/Context.js
+++ src/Context.js
@@ -1,19 +1,17 @@
 import React, {useState, useEffect} from "react"
+
 const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
-
+    
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
-
     useEffect(() => {
         fetch(url)
-            .then(response => response.json())
+            .then(res => res.json())
             .then(data => setAllPhotos(data))
     }, [])
-
-    console.log(allPhotos)
-
+    
     return (
         <Context.Provider value={{allPhotos}}>
             {children}

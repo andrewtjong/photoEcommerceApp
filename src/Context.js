3a28681e6316e439a4838ce84d894e76b127b182
andrewtjong <andrewttj@yahoo.com>
2020-11-10 12:55:01 +0100

    remove item from cart

--- src/Context.js
+++ src/Context.js
@@ -12,25 +12,29 @@ function ContextProvider({children}) {
             .then(res => res.json())
             .then(data => setAllPhotos(data))
     }, [])
-
+    
     function toggleFavorite(id) {
-        const updatedArr = allPhotos.map(photo =>{
+        const updatedArr = allPhotos.map(photo => {
             if(photo.id === id) {
                 return {...photo, isFavorite: !photo.isFavorite}
             }
             return photo
         })
+        
         setAllPhotos(updatedArr)
     }
-
+    
     function addToCart(newItem) {
         setCartItems(prevItems => [...prevItems, newItem])
-
     }
-    console.log(cartItems)
     
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+        
+    }
+
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, addToCart}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, cartItems, addToCart, removeFromCart}}>
             {children}
         </Context.Provider>
     )
